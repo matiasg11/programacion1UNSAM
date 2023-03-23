edc73fb82c4c9e661e0fb7545df9af27021b4ca2
Matias Guarnera <matiasguarnera@gmail.com>
2023-03-22 21:31:54 -0300

    costo_camion para missing.csv con try-except

--- Notas/ejercicios_python/Clase02/costo_camion.py
+++ Notas/ejercicios_python/Clase02/costo_camion.py
@@ -6,10 +6,16 @@ def costo_camion(nombre_archivo):
         costo_total = 0
         for line in f:
             row = line.split(",")
-            costo_total = costo_total + int(row[1])*float(row[2])
+            try:
+                costo_total = costo_total + int(row[1])*float(row[2])
+            except ValueError:
+                print(f'Falta precio para {row[0]}')
             # print(row)
     return costo_total
 
 
 costo = costo_camion('../Data/camion.csv')
 print(f'El costo total fue ${costo}')
+
+costo_missing = costo_camion('../Data/missing.csv')
+print(f'El costo total fue ${costo_missing}')
