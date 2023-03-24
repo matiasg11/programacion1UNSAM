0808c37be8fff0ac433c0d938135946ad2d37fe2
Matias Guarnera <matiasguarnera@gmail.com>
2023-03-24 11:50:49 -0300

    Informe2 listo. Con objetos

---
+++ Notas/ejercicios_python/Clase03/informe2.py
@@ -0,0 +1,50 @@
+import csv
+from pprint import pprint
+
+
+def leer_camion(nombre_archivo):
+    with open(nombre_archivo, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        camion = []
+        for i, row in enumerate(rows):
+            try:
+                obj = {}
+                ncajones = int(row[1])
+                precio = float(row[2])
+                obj["nombre"] = row[0]
+                obj["cajones"] = ncajones
+                obj["precio"] = precio
+                camion.append(obj)
+            except ValueError:
+                print('Faltan datos en la línea', i, "del archivo")
+        return camion
+
+
+este_camion = leer_camion("../Data/camion.csv")
+pprint(este_camion)
+# [('Lima', 100, 32.2), ('Naranja', 50, 91.1), ('Caqui', 150, 103.44), ('Mandarina', 200, 51.23), ('Durazno', 95, 40.37), ('Mandarina', 50, 65.1), ('Naranja', 100, 70.44)]
+
+#
+#
+# def costo_camion(nombre_archivo):
+#     f = open(nombre_archivo, 'rt')
+#     rows = csv.reader(f)
+#     headers = next(rows)
+#     costo_total = 0
+#     for row in rows:
+#         try:
+#             costo_total = costo_total + int(row[1])*float(row[2])
+#         except ValueError:
+#             print(f'Falta precio para {row[0]}')
+
+#         # print(row)
+#     f.close()
+#     return costo_total
+
+
+# costo = costo_camion('../Data/camion.csv')
+# print(f'El costo total fue ${costo}')
+
+# costo_missing = costo_camion('../Data/missing.csv')
+# print(f'El costo total fue ${costo_missing}')
