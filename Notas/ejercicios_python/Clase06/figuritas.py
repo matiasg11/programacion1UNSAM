d0426bc4033746bc01b6c2add41c75d72e7c7a15
Matias Guarnera <matiasguarnera@gmail.com>
2023-04-15 11:08:57 -0300

    promedio para 1000 rep de 6 figus

--- Notas/ejercicios_python/Clase06/figuritas.py
+++ Notas/ejercicios_python/Clase06/figuritas.py
@@ -15,11 +15,33 @@ def crear_album(numero_figuritas):
     return np.zeros(numero_figuritas)
 
 
-album = crear_album(n)
+# album = crear_album(n)
 
 
 def album_incompleto(A):
     return (0 in A)
 
 
-print(album_incompleto(album))
+def comprar_figu(figus_total):
+    return random.randint(1, figus_total)
+
+
+def cuantas_figus(figus_total):
+    album = crear_album(figus_total)
+    compradas = 0
+    while album_incompleto(album):
+        figu = comprar_figu(figus_total)
+        album[figu-1] += 1
+        compradas += 1
+    # print(compradas)
+    return compradas
+
+
+# cuantas_figus(n)
+n_repeticiones = 1000
+resultados = [cuantas_figus(6) for i in range(n_repeticiones)]
+# for i in range(n_repeticiones):
+#     resultados.append(cuantas_figus(6))
+
+resultados = np.array(resultados)
+print(np.mean(resultados))
