d06e60c9772c38f83058c58ea20058e361bb8e7d
Matias Guarnera <matiasguarnera@gmail.com>
2023-04-18 07:26:54 -0300

    Agregado opcion de encabezados. Ahora viene la prueba

--- Notas/ejercicios_python/Clase07/fileparse.py
+++ Notas/ejercicios_python/Clase07/fileparse.py
@@ -1,22 +1,25 @@
 import csv
 
 
-def parse_csv(nombre_archivo, select=None, types = None):
+def parse_csv(nombre_archivo, select=None, types=None, has_headers=True):
     '''
     Parsea un archivo CSV en una lista de registros.
-        Se puede seleccionar sólo un subconjunto de las columnas, determinando el parámetro select, que debe ser una lista de nombres de las columnas a considerar.
+    Se puede seleccionar sólo un subconjunto de las columnas, determinando el parámetro select, que debe ser una lista de nombres de las columnas a considerar.
+    Se puede convertir el tipo directamente con una lista de los tipos requeridos para cada elemento.
     '''
     with open(nombre_archivo) as f:
         rows = csv.reader(f)
 
+        # Declara los índices por única vez
+        indices = []
         # Lee los encabezados
-        headers = next(rows)
+        if has_headers:
+            headers = next(rows)
 
-        if select:
-            indices = [encabezados.index(nombre_columna) for nombre_columna in select]
-            encabezados = select
-        else:
-            indices = []
+            if select:
+                indices = [headers.index(nombre_columna)
+                           for nombre_columna in select]
+                headers = select
 
         registros = []
         for row in rows:
@@ -24,7 +27,13 @@ def parse_csv(nombre_archivo, select=None, types = None):
                 continue
             if indices:
                 row = [row[index] for index in indices]
-            registro = dict(zip(headers, row))
+            if types:
+                row = [tipo(valor) for tipo, valor in zip(types, row)]
+
+            if has_headers:
+                registro = dict(zip(headers, row))
+            else:
+                registro = (row[i] for i in len(row))
             registros.append(registro)
 
     return registros
