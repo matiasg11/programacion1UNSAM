77de45235c031282264094f47b1a4a9b1022b814
Matias Guarnera <matiasguarnera@gmail.com>
2023-04-29 20:41:25 -0300

    "."

--- Notas/ejercicios_python/Clase08/informe_final.py
+++ Notas/ejercicios_python/Clase08/informe_final.py
@@ -8,39 +8,48 @@ Created on Tue Sep  7 12:34:08 2021
 # tabla_informe.py
 import csv
 import fileparse
+
 # %%
 
 
 def leer_camion(nombre_archivo):
-    '''Computa el precio total del camion (cajones * precio) de un archivo'''
+    """Computa el precio total del camion (cajones * precio) de un archivo"""
     camion = fileparse.parse_csv(nombre_archivo)
     return camion
+
+
 # %%
 
 
 def leer_precios(nombre_archivo):
     precios = fileparse.parse_csv(nombre_archivo)
     return precios
+
+
 # %%
 
 
 def hacer_informe(camion, precios):
     lista = []
     for lote in camion:
-        precio_venta = precios['nombre']
-        cambio = precio_venta - lote['precio']
-        t = (lote['nombre'], lote['cajones'], lote['precio'], cambio)
+        precio_venta = precios["nombre"]
+        cambio = precio_venta - lote["precio"]
+        t = (lote["nombre"], lote["cajones"], lote["precio"], cambio)
         lista.append(t)
     return lista
+
+
 # %%
 
 
 def imprimir_informe(informe):
-    print('    Nombre    Cajones     Precio     Cambio')
-    print('---------- ---------- ---------- ----------')
+    print("    Nombre    Cajones     Precio     Cambio")
+    print("---------- ---------- ---------- ----------")
     for nombre, cajones, precio, cambio in informe:
-        precio = f'${precio}'
-        print(f'{nombre:>10s} {cajones:>10d} {precio:>10s} {cambio:>10.2f}')
+        precio = f"${precio}"
+        print(f"{nombre:>10s} {cajones:>10d} {precio:>10s} {cambio:>10.2f}")
+
+
 # %%
 
 
@@ -52,3 +61,8 @@ def informe_camion(nombre_archivo_camion, nombre_archivo_precios):
 
 
 # informe_camion('../Data/camion.csv', '../Data/precios.csv')
+
+
+if __name__ == __main__:
+    if len(sys.argv) > 2:
+        rebotes(int(sys.argv[1]), int(sys.argv[2]))
