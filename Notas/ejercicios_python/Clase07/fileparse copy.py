d05a5871108ce6aff4fafb126336326abfa56eb3
Matias Guarnera <matiasguarnera@gmail.com>
2023-04-30 20:09:39 -0300

    clean and add

---
+++ Notas/ejercicios_python/Clase07/fileparse copy.py
@@ -0,0 +1,40 @@
+import csv
+
+
+def parse_csv(nombre_archivo, select=None, types=None, has_headers=True):
+    '''
+    Parsea un archivo CSV en una lista de registros.
+    Se puede seleccionar sólo un subconjunto de las columnas, determinando el parámetro select, que debe ser una lista de nombres de las columnas a considerar.
+    Se puede convertir el tipo directamente con una lista de los tipos requeridos para cada elemento.
+    '''
+    with open(nombre_archivo) as f:
+        rows = csv.reader(f)
+
+        # Declara los índices por única vez
+        indices = []
+        # Lee los encabezados
+        if has_headers:
+            headers = next(rows)
+
+            if select:
+                indices = [headers.index(nombre_columna)
+                           for nombre_columna in select]
+                headers = select
+
+        registros = []
+        for row in rows:
+            # Saltea filas sin datos
+            if not row or ("" in row):
+                continue
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [tipo(valor) for tipo, valor in zip(types, row)]
+
+            if has_headers:
+                registro = dict(zip(headers, row))
+            else:
+                registro = tuple([row[i] for i in range(len(row))])
+            registros.append(registro)
+
+    return registros
---
+++ Notas/ejercicios_python/Clase07/informe_funciones copy.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 12:34:08 2021
+
+@author: mcerdeiro
+"""
+# tabla_informe.py
+import csv
+import fileparse
+# %%
+
+
+def leer_camion(nombre_archivo):
+    '''Computa el precio total del camion (cajones * precio) de un archivo'''
+    camion = fileparse.parse_csv(nombre_archivo)
+    return camion
+# %%
+
+
+def leer_precios(nombre_archivo):
+    precios = fileparse.parse_csv(nombre_archivo)
+    return precios
+# %%
+
+
+def hacer_informe(camion, precios):
+    lista = []
+    for lote in camion:
+        precio_venta = precios['nombre']
+        cambio = precio_venta - lote['precio']
+        t = (lote['nombre'], lote['cajones'], lote['precio'], cambio)
+        lista.append(t)
+    return lista
+# %%
+
+
+def imprimir_informe(informe):
+    print('    Nombre    Cajones     Precio     Cambio')
+    print('---------- ---------- ---------- ----------')
+    for nombre, cajones, precio, cambio in informe:
+        precio = f'${precio}'
+        print(f'{nombre:>10s} {cajones:>10d} {precio:>10s} {cambio:>10.2f}')
+# %%
+
+
+def informe_camion(nombre_archivo_camion, nombre_archivo_precios):
+    camion = leer_camion(nombre_archivo_camion)
+    precios = leer_precios(nombre_archivo_precios)
+    informe = hacer_informe(camion, precios)
+    imprimir_informe(informe)
+
+
+# informe_camion('../Data/camion.csv', '../Data/precios.csv')
