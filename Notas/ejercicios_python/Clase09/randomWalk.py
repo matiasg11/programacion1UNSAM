473f739f1fd46540db8712a38bc04129b73b16e1
Matias Guarnera <matiasguarnera@gmail.com>
2023-05-20 09:33:18 -0300

    Calculo de los que más se alejan y los que menos

--- Notas/ejercicios_python/Clase09/randomWalk.py
+++ Notas/ejercicios_python/Clase09/randomWalk.py
@@ -9,8 +9,18 @@ N=1000
 caminantes = []
 for i in range(12):
     caminantes.append(randomwalk(N))
-
+   
+maximos = []
 tiempo = np.array(range(0, N))
 for caminante in caminantes:
-    plt.plot(tiempo, caminante)
-plt.show()
+    print(caminante.max(), caminante.min())
+    maximos.append(max(caminante.max(), abs(caminante.min())))
+    plt.plot(tiempo, caminante, color=np.random.rand(3,), linewidth=1.0, linestyle="-", label=i+1)
+
+print(maximos)
+print("El que más se aleja es el caminante", maximos.index(max(maximos))+1, "que llega hasta la posición", max(maximos))
+print("El que menos se aleja es el caminante", maximos.index(min(maximos))+1, "que llega hasta la posición", min(maximos))
+
+
+
+#plt.show()
