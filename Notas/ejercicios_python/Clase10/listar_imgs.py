240154e73a0fe5f08fe837ce5b0f990ebca4e8e9
Matias Guarnera <matiasguarnera@gmail.com>
2023-06-18 19:56:30 -0300

    Continuando el ejercicio de ordenamiento

--- Notas/ejercicios_python/Clase10/listar_imgs.py
+++ Notas/ejercicios_python/Clase10/listar_imgs.py
@@ -3,12 +3,16 @@ import datetime
 import time
 
 
-def archivos_png(directorio):
+def archivos_png(directorio, debugger=False):
     """Arma una lista de todos los archivos .png que se encuentren en algún directorio o subdirectorio dado"""
+    lista_archivos = []
     for root, dirs, files in os.walk(directorio):
         for name in files:
             if ".png" in name:
-                print(os.path.normpath(os.path.join(root, name)))
+                lista_archivos.append({"root": root, "name": name})
+                if debugger:
+                    print(os.path.normpath(os.path.join(root, name)))
+    return lista_archivos
 
 
-archivos_png("../Data/ordenar")
+# archivos_png("../Data/ordenar")
--- Notas/ejercicios_python/Clase10/ordenar_imgs.py
+++ Notas/ejercicios_python/Clase10/ordenar_imgs.py
@@ -1,9 +1,15 @@
 import os
+from listar_imgs import archivos_png
+from datetime import datetime
+import time
 
 
 # Crear un nuevo directorio '../Data/imgs_procesadas/'
 def crear_carpeta(directorio, nombre_carpeta):
-    os.mkdir(os.path.join(directorio, nombre_carpeta))
+    if nombre_carpeta in os.listdir(directorio):
+        print("La carpeta existe")
+    else:
+        os.mkdir(os.path.join(directorio, nombre_carpeta))
 
 
 directorio = "../Data"
@@ -11,10 +17,40 @@ carpeta_a_crear = "imgs_procesadas"
 crear_carpeta(directorio, carpeta_a_crear)
 
 # Buscar archivos png en la carpeta deseada
+carpeta_de_busqueda = "ordenar"
+path_carpeta_de_busqueda = os.path.join(directorio, carpeta_de_busqueda)
+lista_archivos_png = archivos_png(os.path.join(path_carpeta_de_busqueda))
 
 # Leer fecha de los últimos 8 digitos del archivo
 
+# Formato de archivo: "nombrefinal_YYYYMMDD.png"
 # Modificar fecha de edicion y acceso
+
+
+def mod_fecha_edicion(archivo, fecha_nueva_str):
+    fecha_datetime = datetime.strptime(fecha_nueva_str, "%Y%m%d")
+    fecha_timestamp = fecha_datetime.timestamp()
+    os.utime(archivo, (fecha_timestamp, fecha_timestamp))
+
+
+def extraer_datos(archivo):
+    nombre = str(archivo["name"])
+    extension = nombre[-4:-1] + nombre[-1]
+    fecha = nombre[-12:-4]
+    nombre_nuevo = nombre[0:-13]
+    # Debugger
+    print(nombre, extension, fecha, nombre_nuevo)
+    return tuple((nombre_nuevo, fecha, extension))
+
+
+# def procesar_nombre(fname):
+
+for archivo in lista_archivos_png:
+    (nombre_nuevo, fecha, extension) = extraer_datos(archivo)
+    mod_fecha_edicion(
+        os.path.normpath(os.path.join(archivo["root"], archivo["name"])), fecha
+    )
+
 
 # Cambiar el nombre del archivo
 
